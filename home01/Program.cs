735be5e4a2e3e7fe61d92ade1096349a37b9a916
Rayenire <aeristarg@gmail.com>
2022-07-24 19:13:38 +0300

    Add home01

---
+++ home01/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void AvgMatrix(int[, ] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+void CreateMatrix(int[, ] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 101);
+    }
+  }
+}
+
+
+void WriteMatrix(int[, ] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+int[,] matrix = new int[4, 4];
+
+CreateMatrix(matrix);
+WriteMatrix(matrix);
+
+Console.WriteLine();
+
+AvgMatrix(matrix);
+WriteMatrix(matrix);
